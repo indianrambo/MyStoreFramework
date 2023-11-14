13a48f51d6ee6b4417ce2a10d1c27038f1dbdf50
Manoj <manojpradhan268@gmail.com>
2023-11-14 08:36:32 +0530

    dashboard verified

--- src/test/java/com/mystore/testcases/TC_VerifyDashBoardpage.java
+++ src/test/java/com/mystore/testcases/TC_VerifyDashBoardpage.java
@@ -1,17 +1,17 @@
 package com.mystore.testcases;
 
+import org.testng.annotations.Test;
 import com.mystore.pageobject.DashBoardPage;
 import com.mystore.pageobject.FormPage;
 import com.mystore.utilities.BaseClass;
 
-public class TC_DashBoard extends BaseClass{
+public class TC_VerifyDashBoardpage extends BaseClass{
 
-	DashBoardPage dashpage;
 	
-	public void verifyTitle() {
-    
+	@Test
+	public void verifyDashboardPage() {
 		FormPage page = new FormPage(driver);
-		dashpage = new DashBoardPage(driver);
+		DashBoardPage dashpage = new DashBoardPage(driver);
 		driver.get(url);
 		page.enterEmail("admin@yourstore.com");
 		page.enterPassword("admin");
